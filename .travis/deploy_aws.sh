0fe0559ae4d9eab18007d05b2330bec1496f3165
Vlad <47606000+Suntory22@users.noreply.github.com>
2019-02-28 19:24:54 +0200

    Update deploy_aws.sh

--- .travis/deploy_aws.sh
+++ .travis/deploy_aws.sh
@@ -4,7 +4,7 @@ region: "eu-west-2"
 app: "devopswin"
 env: "Devopswin-env"
 bucket_name: "elasticbeanstalk-us-east-2-958499216158"
-bucket_path: "devopswin"
+bucket_path: "https://s3.us-east-2.amazonaws.com/elasticbeanstalk-us-east-2-958499216158/.elasticbeanstalk"
 on:
   branch: master
 access_key_id: $AWS_ACCESS_KEY
