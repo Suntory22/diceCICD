3c814af59e27434487e59d1840282342b8285d22
Vlad <47606000+Suntory22@users.noreply.github.com>
2019-02-28 15:02:12 +0200

    Delete deploy_heroku.sh

--- .travis/deploy_heroku.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -e=$DOCKER_EMAIL -p=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
