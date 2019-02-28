1460a89f708b7d9e2f69b64159f91753b7c081a1
Vlad <47606000+Suntory22@users.noreply.github.com>
2019-02-28 15:46:10 +0200

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  TAG = "latest"
+if [ "$TRAVIS_BRANCH"="master" ]; then
+  TAG="latest"
 else
-  TAG = "$TRAVIS_BRANCH"
+  TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $DOCKER_REPO
