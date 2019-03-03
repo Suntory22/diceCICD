0226e59fcbbc525d668fc61bbfc9602fb16a5ef1
Vlad <47606000+Suntory22@users.noreply.github.com>
2019-03-03 17:21:38 +0200

    Update deploy_aws.sh

--- .travis/deploy_aws.sh
+++ .travis/deploy_aws.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-provider: elasticbeanstalk
-region: "eu-west-2"
-app: "devopswin"
-env: "Devopswin-env"
-bucket_name: "elasticbeanstalk-us-east-2-958499216158"
-bucket_path: "http://devopswin-env.hzgjwmnsit.us-east-2.elasticbeanstalk.com/"
-on:
-  branch: master
-access_key_id: $AWS_ACCESS_KEY
-secret_access_key:
-  secure: "$AWS_SECRET_KEY"
+#provider: elasticbeanstalk
+#region: "eu-west-2"
+#app: "devopswin"
+#env: "Devopswin-env"
+#bucket_name: "elasticbeanstalk-us-east-2-958499216158"
+#bucket_path: "http://devopswin-env.hzgjwmnsit.us-east-2.elasticbeanstalk.com/"
+#on:
+#  branch: master
+#access_key_id: $AWS_ACCESS_KEY
+#secret_access_key:
+#  secure: "$AWS_SECRET_KEY"
